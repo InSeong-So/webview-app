fadc081428ac7211f73cf0064d384576c3efe510
InSeong-So <goflvhxj2547@gmail.com>
2023-01-06 18:52:14 +0900

    Feat: WebView 띄워보기

---
+++ app/containers/SafeAreaView.tsx
@@ -0,0 +1,34 @@
+import { memo, ReactElement} from 'react';
+import {StyleSheet, ViewProps} from "react-native";
+import { SafeAreaView as SafeAreaContext } from 'react-native-safe-area-context';
+
+import { themes } from '../lib/constants';
+import { useTheme } from '../theme';
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1
+    }
+});
+
+type SupportedChildren = ReactElement | ReactElement[] | null;
+type TSafeAreaViewChildren = SupportedChildren | SupportedChildren[];
+
+interface ISafeAreaView extends ViewProps {
+    vertical?: boolean;
+    children: TSafeAreaViewChildren;
+}
+
+const SafeAreaView = memo(({ style, children, vertical = true, ...props }: ISafeAreaView) => {
+    const { theme } = useTheme();
+    return (
+        <SafeAreaContext
+            style={[styles.view, { backgroundColor: themes[theme].auxiliaryBackground }, style]}
+            edges={vertical ? ['right', 'left'] : undefined}
+            {...props}>
+            {children}
+        </SafeAreaContext>
+    );
+});
+
+export default SafeAreaView;
---
+++ app/definitions/TNavigation.ts
@@ -0,0 +1,13 @@
+import { StackNavigationOptions } from '@react-navigation/stack';
+
+
+interface INavigationProps {
+    route?: any;
+    navigation?: any;
+    isMasterDetail?: boolean;
+}
+
+export type TNavigationOptions = {
+    navigationOptions?(props: INavigationProps): StackNavigationOptions;
+};
+
